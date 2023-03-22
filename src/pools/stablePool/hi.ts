2212f892561c45f9ff1e0edabec7d206d3dcdf3b
Dmitry Zadorozhniy <61386841+swel4ik@users.noreply.github.com>
2023-03-22 23:31:26 +0300

    Update hi.ts

--- src/pools/stablePool/hi.ts
+++ src/pools/stablePool/hi.ts
@@ -608,9 +608,9 @@ export function _derivativeSpotPriceAfterSwapTokenInForExactTokenOut(
 }
 const amp = BigNumber.from("1472000");
 const balances = [
-    OldBigNumber.from("5.4271966830814916e+23"),
-    OldBigNumber.from("4.4188939033653407e+23"),
-    OldBigNumber.from("3.9213291871146253e+23"),
+    new OldBigNumber("5.4271966830814916e+23"),
+    new OldBigNumber("4.4188939033653407e+23"),
+    new OldBigNumber("3.9213291871146253e+23"),
 ]
 "1.111222233334444555566e+21"
 console.log(
